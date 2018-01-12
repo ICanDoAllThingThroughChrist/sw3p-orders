bc7232776401b4467bb83c9a841241cbf87347d0
github username <github email address>
2018-01-11 22:35:37 -0600

    provided authenticity token, created nested form, whitelisted params method written" ;

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,3 +1,17 @@
 class SitesController < ApplicationController
+    def index
+        @site = Site.all
+    end
+    def new 
+        @site = Site.new 
+    end
+    def create
+        binding.pry
+    end 
 
+    private 
+    def site_params
+        params.require(:site).permit(:name, task: [:name])
+    end 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   resources :orders
-  resources :sites do
-    resources :tasks 
-  end 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-
   resources :users, only: [:new, :create]
-
+  resources :sites, only: [:index, :new, :create]
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
