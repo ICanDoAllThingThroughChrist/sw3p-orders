21528060930cb3e380b7bd5550e6c9582dbee7b6
github username <github email address>
2018-01-20 16:35:31 -0600

    implemented model class methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,22 @@
 class TasksController < ApplicationController
+    
     def index
-        if params[:user_id]
-            @users = User.find(params[:user_id]).tasks
+  # provide a list of users to the view for the filter control
+        @users = User.all   
+        # filter the @tasks list based on user input
+        if !params[:user].blank?
+            @tasks = Task.by_user(params[:user])
+        elsif !params[:date].blank?
+            if params[:date] == "Today"
+            @tasks = Task.from_today
+            else
+            @tasks = Task.old_news
+            end
         else
+            # if no filters are applied, show all tasks
             @tasks = Task.all
-        end 
-    end 
+        end
+    end
     
     def show
         @task = Task.find(params[:id])
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,15 @@ class Task < ApplicationRecord
     has_many :orders
     has_many :site, through: :orders 
     belongs_to :user
+    def self.by_user(user_id)
+        where(user: user_id)
+    end
+    def self.from_today
+        where("created_at >=?", Time.zone.today.beginning_of_day)
+    end
+       
+    def self.old_news
+        where("created_at <?", Time.zone.today.beginning_of_day)
+    end
 end
 
