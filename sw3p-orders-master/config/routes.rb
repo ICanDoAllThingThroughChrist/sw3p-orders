11ddaaa9e9dffe3bc9a74d128a2d12feb84e8dad
github username <github email address>
2018-02-01 21:34:29 -0600

    omnisolved

--- sw3p-orders-master/config/routes.rb
+++ sw3p-orders-master/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get  '/signup',     to: 'users#new'
   post '/signup',     to: 'users#create'
-  get    '/login',    to: 'sessions#new'  #cRud = 'GET login to READ sessions#new form & controller'
+  get    '/login/',    to: 'sessions#new'  #cRud = 'GET login to READ sessions#new form & controller'
   post   '/login',    to: 'sessions#create'#Crud = 'Post data from login page to Create new record'
   delete '/logout',   to: 'sessions#destroy'
   resources :sites, only: [:index, :new, :create, :show] do 
