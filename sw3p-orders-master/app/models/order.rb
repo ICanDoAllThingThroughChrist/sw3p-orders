1a3243f19201b74816777004153b68ea50ae3f84
github username <github email address>
2018-02-15 22:29:17 -0600

    completed-edit-update-for-orders-comment

--- sw3p-orders-master/app/models/order.rb
+++ sw3p-orders-master/app/models/order.rb
@@ -1,13 +1,21 @@
 class Order < ApplicationRecord #Class Survey
-    # before_save :create_comment, :save_comment_names
-    # attr_accessor :new_comment, :comment_names
-    # attr_writer :comment_names
+    before_save :save_comment_names
+    attr_accessor :comment_names
+    attr_writer :comment_names
     belongs_to :user
     belongs_to :site#
     has_many :ordercomments
     has_many :comments, through: :ordercomments #questions
     accepts_nested_attributes_for :comments, allow_destroy: true
-
+    def save_comment_names
+        if @comment_names 
+            binding.pry
+            self.comments = @comment_names.split.map { |comment| Comment.where(comment: comment).first_or_create!}
+        end
+    end  
+    def comment_names 
+        @comment_names || comments.pluck(:comment).join(' ')
+    end 
     def self.by_author(user_id)
         where(user:user_id) 
     end
