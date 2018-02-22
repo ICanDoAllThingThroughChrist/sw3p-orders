9df8bf10f3dd9ed8ba06f44eb3a8da340865205e
github username <github email address>
2018-02-21 20:59:34 -0600

    SiteOrder#Create-#Update-Done

--- sw3p-orders-master/app/models/order.rb
+++ sw3p-orders-master/app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord #Class Survey
-    before_save :save_comment_names
-    attr_accessor :comment_names
-    attr_writer :comment_names
+    #before_save :save_comment_names
+    #attr_accessor :comment_names
+    #attr_writer :comment_names
     belongs_to :user
     belongs_to :site#
     has_many :ordercomments
@@ -17,32 +17,41 @@ class Order < ApplicationRecord #Class Survey
         where(name: 'task2')
     end
         
-    def save_comment_names
-        if @comment_names 
-            binding.pry
-            self.comments = @comment_names.split.map { |comment| Comment.where(comment: comment).first_or_create!}
-        end
-    end  
-    def comment_names 
-        @comment_names || comments.pluck(:comment).join(' ')
-    end 
+    # def save_comment_names
+    #     if @comment_names 
+    #         binding.pry
+    #         self.comments = @comment_names.split.map { |comment| Comment.where(comment: comment).first_or_create!}
+    #     end
+    # end  
+    # def comment_names 
+    #     @comment_names || comments.pluck(:comment).join(' ')
+    # end 
     def self.by_author(user_id)
         where(user:user_id) 
     end
     def comments_attributes=(comment_attributes)
         #binding.pry
         comment_attributes.each do |index, value|
-            binding.pry
+            #binding.pry
             if value[:comment] == nil 
                 new_comment = Comment.create(comment: 'a comment')
                 self.comments << new_comment 
             else 
                if !value[:comment].empty?
-                    if new_comment = Comment.find_by(comment: value[:comment])
+                    if new_comment= Comment.find_by(comment: value[:comment])
                     binding.pry
-                    self.comments << new_comment 
+                    a= []
+                    self.comments.each {|comment| a << "#{comment.comment}"}
+                    b= a.join(" ,")
+                    self.comments << Comment.create(comment: b) 
                     else
-                    self.comments.build(comment: value[:comment])
+                    binding.pry
+                    new_comment = Comment.create(comment: value[:comment])
+                    self.comments << new_comment
+                    new_comment2= self.comments.pluck(:comment).join(' ')
+                    binding.pry
+                    new_comment3= Comment.create(comment: new_comment2)
+                    self.comments << new_comment3
                     end 
                end
            end 
